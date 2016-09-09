3aa97e9fd1e07b923dc9ce6dc980c3f25aa57931
Hanneybadger <hwillen@gmail.com>
2016-09-09 11:59:52 +0200

    GETTING THE RIGHT DATA
    not visualising it though.

--- simple_flask_server.py
+++ simple_flask_server.py
@@ -24,121 +24,28 @@ static_folder = os.path.join(proj_dir, 'static')
 app = Flask(__name__)
 
 
-def get_photos(hashtag):
+def get_photos():
     '''
     This method queries Instagram 'media/search' 
     (https://www.instagram.com/developer/endpoints/media/)
     API endpoint for a given lati/longi and returns the reply as a json()
     '''
-    insta_address = "https://api.instagram.com/v1/tags/search?q=tjejerkodar&access_token={}".format(INSTA_ACCESS_TOKEN)
+    insta_address = "https://api.instagram.com/v1/tags/100tjejerkodar/media/recent?access_token={}".format(INSTA_ACCESS_TOKEN)
 
     photos = requests.get(insta_address)
     
     return photos.json()
 
-def _get_hashtag(insta_address):
+# def _get_hashtag(insta_address):
 
-    '''
-    I want this to define an instagramhashtag that the get_photos-function can use instead of lati, longi.
-    '''
-    # insta_address = "https://api.instagram.com/v1/tags/search?q=tjejerkodar&access_token={}".format(INSTA_ACCESS_TOKEN)
+#     '''
+#     I want this to define an instagramhashtag that the get_photos-function can use instead of lati, longi.
+#     '''
+#     insta_address = "https://api.instagram.com/v1/tags/search?q=tjejerkodar&access_token={}".format(INSTA_ACCESS_TOKEN)
 
-    # hashtag = request.get(insta_address)
+#     hashtag = request.get(insta_address)
 
-    # return hashtag.json()
-
-    insta_tag = "https://api.instagram.com/v1/tags/search?q={}&access_token={}".format(tjejerkodar, INSTA_ACCESS_TOKEN)
-
-    return insta_tag.json()
-
-
-def _get_weather(lati, longi):
-    '''
-    This function queries the forcast.io '/forcast' API endpoint for
-    a given lati/longi and processes the data to return a single string
-    which is a description of the current temperature and summary of
-    daily forecast
-    '''
-    r = requests.get('https://api.forecast.io/forecast/{}/{},{}'.format(WEATHER_API_KEY, lati, longi))
-
-    d = r.json()
-
-    return d["currently"]["summary"]
-
-
-def _get_lati_longi(address):
-    '''
-    This function takes an address string and return the lati/longi for
-    that address
-    '''
-    # lati = None
-    # longi = None
-
-    g = geocoder.google(address).latlng
-
-    lati = g[0]
-    longi = g[1]
-    '''
-    Calcuelate  lati / longi...
-    '''
-    return lati, longi
-
-
-def _save_static_file(name, content):
-    '''
-    This fucntion takes a name of a file to create in the
-    /static folder (which can be used to load images) and
-    write the 'content' given to it.
-
-    This is useful if an API gives you a binary content
-    '''
-
-    filename = os.path.join(static_folder, name)
-
-    # Delete it if exists
-    try:
-        os.remove(filename)
-    except OSError:
-        pass
-
-    # Write content to file
-    with open(filename, 'w+b') as f:
-        f.write(content)
-
-
-def get_static_map(lati, longi):
-    '''
-    This function queries the google maps '/staticmap' API endpoint for
-    a given lati/longi gets a static map image. Since the API returns
-    the image itself, this funciton need to save that image to disk in
-    the /static folder and then return a STRING which is
-    /static/<image_name>?<random_number>
-    The latter is to avoid browser caching.
-    '''
-    # r = requiests.get(...)
-    r = requests.get('https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom=14&size=600x400&scale=1&key={}'.format(lati, longi, GOOGLE_MAPS_STATIC_API_KEY))
-    _save_static_file('map.jpg', r.content)  # r is the result of requests.get() from the google static maps API
-
-    return '/static/map.jpg?{}'.format(time.time())
-    
-
-
-def get_streetview(lati, longi):
-    '''
-    This function queries the google maps '/streetview' API endpoint for
-    a given lati/longi gets a static map image. Since the API returns
-    the image itself, this funciton need to save that image to disk in
-    the /static folder and then return a STRING which is
-    /static/<image_name>?<random_number>
-    The latter is to avoid browser caching.
-    '''
-
-    # r = requiests.get(...)
-    r = requests.get('https://maps.googleapis.com/maps/api/streetview?size=600x400&location={},{}&key{}'.format(lati, longi, GOOGLE_MAPS_STREETVIEW_API_KEY))
-    
-    _save_static_file('street.jpg', r.content)  # r is the result of requests.get() from the google streetview API
-
-    return '/static/street.jpg?{}'.format(time.time())
+#     return hashtag.json()
 
 
 @app.route('/')
@@ -147,24 +54,17 @@ def index():
 
 @app.route('/describe/<address>')
 def describe_address(address):
-    lati, longi = _get_lati_longi(address)
-
-    hashtag = _get_hashtag(hashtag)
-
-    # This is just text describing the weather
-    weather = _get_weather(lati, longi)
 
     # This is a list jsons which has a )
-    photos_urls = get_photos(hashtag)
-
-    # These are filenames for a jpg file containing the image
-    static_map = get_static_map(lati, longi)
-    street_view = get_streetview(lati, longi)
-
+    photos_urls = get_photos()
+    print "\n\n\n\n\n"
+    print photos_urls
+    print "\n\n\n\n\n"
+    raise
     return render_template('./address.html', address=address.capitalize(),
-                           lati=lati, longi=longi, weather=weather,
-                           photos=photos_urls, static_map=static_map,
-                           street_view=street_view),
+                           lati=None, longi=None, weather=None,
+                           photos=photos_urls, static_map=None,
+                           street_view=None), 
 
 
 if __name__ == "__main__":
