40f2d3177edf92297aa8b9f637e49040c1a4d762
Hanneybadger <hwillen@gmail.com>
2016-09-09 12:11:58 +0200

    its visualized
    i can now search for a hashtag and i can actually see the most recent
    photos of that #

--- simple_flask_server.py
+++ simple_flask_server.py
@@ -24,48 +24,34 @@ static_folder = os.path.join(proj_dir, 'static')
 app = Flask(__name__)
 
 
-def get_photos():
+def get_photos(hashtag):
     '''
     This method queries Instagram 'media/search' 
     (https://www.instagram.com/developer/endpoints/media/)
     API endpoint for a given lati/longi and returns the reply as a json()
     '''
-    insta_address = "https://api.instagram.com/v1/tags/100tjejerkodar/media/recent?access_token={}".format(INSTA_ACCESS_TOKEN)
-
-    photos = requests.get(insta_address)
+    r = requests.get("https://api.instagram.com/v1/tags/{}/media/recent?access_token={}".format(hashtag, INSTA_ACCESS_TOKEN))
     
-    return photos.json()
-
-# def _get_hashtag(insta_address):
-
-#     '''
-#     I want this to define an instagramhashtag that the get_photos-function can use instead of lati, longi.
-#     '''
-#     insta_address = "https://api.instagram.com/v1/tags/search?q=tjejerkodar&access_token={}".format(INSTA_ACCESS_TOKEN)
-
-#     hashtag = request.get(insta_address)
-
-#     return hashtag.json()
+    return r.json()
 
 
-@app.route('/')
-def index():
-    return render_template('./index.html')
-
-@app.route('/describe/<address>')
-def describe_address(address):
-
-    # This is a list jsons which has a )
-    photos_urls = get_photos()
-    print "\n\n\n\n\n"
+@app.route('/describe/<hashtag>')
+def describe_hashtag(hashtag):
+    
+    photos_urls = get_photos(hashtag)
+    print ""
+    print ""
+    print ""
+    print ""
+    print ""
     print photos_urls
-    print "\n\n\n\n\n"
-    raise
-    return render_template('./address.html', address=address.capitalize(),
-                           lati=None, longi=None, weather=None,
-                           photos=photos_urls, static_map=None,
-                           street_view=None), 
-
+    print ""
+    print ""
+    print ""
+    print ""
+    print ""    
+   
+    return render_template('./address.html', photos=photos_urls)
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
